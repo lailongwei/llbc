a7d7ffdd889e7fc60eab46f0fa0dceacc01e61f7
lailongwei <lailongwei@126.com>
2016-09-20 16:03:30 +0800

    Add Windows platform solution file generate script

---
+++ WinPreBuild.bat
@@ -0,0 +1,28 @@
+@echo off
+
+Rem Generate visual studio project files
+Rem This file is a part of llbc library
+
+echo Visual Studio solution and project files generate tool.
+echo For now supported Visual Studio versions:
+echo    vs2005
+echo    vs2008
+echo    vs2010
+echo    vs2012
+echo    vs2013
+echo    vs2015
+set /p choose=Please input:
+
+cd tools\premake && premake5_windows.exe %choose%
+
+if errorlevel 1 (
+    echo Failed to generate Visual Studio solution and project files, error: %errorlevel%
+    pause
+) else (
+    echo Succcess to generate Visual Studio solution and project files
+    echo Solution file path: build/%choose%/llbc_%choose%.sln
+    echo Press any key to open target directory...
+    pause >nul
+    
+    explorer ..\..\build\%choose%
+)
