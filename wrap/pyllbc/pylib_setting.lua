a49768b37c89a9454e66dc162998c13df63f0cbb
lailongwei <lailongwei@126.com>
2017-07-17 09:22:11 +0000

    增加pyllbc编译设定文件，用于显式指定python库信息(incl, lib_dir, libname)

---
+++ wrap/pyllbc/pylib_setting.lua
@@ -0,0 +1,6 @@
+local pylib_setting = {
+    use_setting = false, -- If use_setting is False, compile system will auto detect python
+    py_path = {"", "", ""}, -- python path setting, 1: include path, 2: library path, 3: library name
+}
+
+return pylib_setting
