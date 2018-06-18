19df83fbcb2f1170e6c54539dfe719b4424d22f3
lailongwei <lailongwei@126.com>
2018-06-18 10:46:38 +0800

    Upgrade version to 1.0.6

--- llbc/src/common/Version.cpp
+++ llbc/src/common/Version.cpp
@@ -31,7 +31,7 @@ __LLBC_NS_BEGIN
 
 int LLBC_majorVersion       = 1;
 int LLBC_minorVersion       = 0;
-int LLBC_updateNo           = 5;
+int LLBC_updateNo           = 6;
 
 #ifdef LLBC_DEBUG
 bool LLBC_isDebugVer        = true;
