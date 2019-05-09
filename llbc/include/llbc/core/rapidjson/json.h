e855003a39371f2bfc483ff1aacfeb38b5535bad
huangrongfu <huangrongfu00@gmail.com>
2019-05-09 21:35:21 +0800

    添加描述

--- llbc/include/llbc/core/rapidjson/json.h
+++ llbc/include/llbc/core/rapidjson/json.h
@@ -22,8 +22,18 @@ typedef LLBC_Json::Value::ConstValueIterator LLBC_JsonValueCIter;
 typedef LLBC_Json::Value::MemberIterator LLBC_JsonMemberIter;
 typedef LLBC_Json::Value::ConstMemberIterator LLBC_JsonMemberCIter;
 
+/**
+ * LLBC_JsonValue convert to LLBC_String
+ * @param[in] value - LLBC_JsonValue
+ * @param[out] outStr - output string
+ * @param[in] isPretty - when it is true, then json output a pretty string.
+ */
 LLBC_EXTERN LLBC_EXPORT void LLBC_JsonToString(const LLBC_JsonValue &value, LLBC_String &outStr, bool isPretty = false);
-LLBC_EXTERN LLBC_EXPORT std::ostream &operator <<(std::ostream &o, const LLBC_Json::Value &json);
+
+/**
+ * LLBC_JsonValue output to stream operator function(in global ns).
+ */
+LLBC_EXTERN LLBC_EXPORT std::ostream &operator <<(std::ostream &o, const LLBC_JsonValue &json);
 
 __LLBC_NS_END
 
