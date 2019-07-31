eaf6b5fcea4e37100b7375c893504071b905e2b8
zhouyan <zhouyan@onemt.com.cn>
2019-07-31 19:36:25 +0800

    lullbc.Log: Add `Log.trimFilePrefix` to control trim Log file info prefix character; Use `local` increase Log.lua performance

--- wrap/lullbc/script/core/log/Log.lua
+++ wrap/lullbc/script/core/log/Log.lua
@@ -1,5 +1,13 @@
 local Log = llbc.newclass('llbc.Log')
 
+local _llbc = _llbc
+local type = type
+local string_sub = string.sub
+local debug_getinfo = debug.getinfo
+local debug_traceback = debug.traceback
+local Util_Table_Concat = _llbc.Util_Table_Concat
+local LogMsg = _llbc.LogMsg
+
 -- Log levels enumeration.
 Log.DEBUG = 0
 Log.INFO = 1
@@ -9,6 +17,8 @@ Log.FATAL = 4
 
 -- logFileInfo: to control output message included file&line information or not, default is false.
 Log.logFileInfo = false
+-- trimFilePrefix: to control trim Log file info prefix character(&,=,@,...), default is false.
+Log.trimFilePrefix = false
 -- defaultLogTag: use to fill <tag> param on output message, can set to string or function, default is nil.
 Log.defaultLogTag = nil
 
@@ -21,137 +31,14 @@ end
 -- @param[required] cfgFile     - log init config file.
 -- @param[optional] logFileinfo - log file info or not, default is false.
 -- @returns - no return.
-function Log.init(cfgFile, logFileInfo)
+function Log.init(cfgFile)
     _llbc.InitLog(cfgFile)
-    if logFileInfo then
-        Log.logFileInfo = true
-    end
 end
 
 -- Check Log init or log.
 -- @returns boolean - init flag.
 function Log.isinit()
     return _llbc.IsLogInit()
-end
-
--- Log DEBUG level message to root logger.
--- @param ... - log message.
--- @returns - no return.
-function Log.d(...)
-    Log.output(0, nil, nil, ...)
-end
-
--- Log DEBUG level message to specific logger.
--- @param[optional] logger - logger name, default is root logger.
--- @param ...              - log message.
--- @returns - no return.
-function Log.d2(logger, ...)
-    Log.output(0, logger, nil, ...)
-end
-
--- Log DEBUG level message to specific logger, and append tag.
--- @param[optional] logger - logger name, default is root logger.
--- @param[optional] tag    - log tag, default is nil.
--- @param ...              - log message.
--- @returns - no return.
-function Log.d3(logger, tag, ...)
-    Log.output(0, logger, tag, ...)
-end
-
--- Log INFO level message to root logger.
--- @param ... - log message.
--- @returns - no return.
-function Log.i(...)
-    Log.output(1, nil, nil, ...)
-end
-
--- Log INFO level message to specific logger.
--- @param[optional] logger - logger name, default is root logger.
--- @param ...              - log message.
--- @returns - no return.
-function Log.i2(logger, ...)
-    Log.output(1, logger, nil, ...)
-end
-
--- Log INFO level message to specific logger, and append tag.
--- @param[optional] logger - logger name, default is root logger.
--- @param[optional] tag    - log tag, default is nil.
--- @param ...              - log message.
--- @returns - no return.
-function Log.i3(logger, tag, ...)
-    Log.output(1, logger, tag, ...)
-end
-
--- Log WARN level message to root logger.
--- @param ... - log message.
--- @returns - no return.
-function Log.w(...)
-    Log.output(2, nil, nil, ...)
-end
-
--- Log WARN level message to specific logger.
--- @param[optional] logger - logger name, default is root logger.
--- @param ...              - log message.
--- @returns - no return.
-function Log.w2(logger, ...)
-    Log.output(2, logger, nil, ...)
-end
-
--- Log WARN level message to specific logger.
--- @param[optional] logger - logger name, default is root logger.
--- @param[optional] tag    - log tag, default is nil.
--- @param ... -              log message.
--- @returns - no return.
-function Log.w3(logger, tag, ...)
-    Log.output(2, logger, tag, ...)
-end
-
--- Log ERROR level message to root logger.
--- @param ... - log message.
--- @returns - no return.
-function Log.e(...)
-    Log.output(3, nil, nil, ...)
-end
-
--- Log ERROR level message to specific logger.
--- @param[optional] logger - logger name, default is root logger.
--- @param ...              - log message.
--- @returns - no return.
-function Log.e2(logger, ...)
-    Log.output(3, logger, nil, ...)
-end
-
--- Log ERROR level message to specific logger, and append tag.
--- @param[optional] logger - logger name, default is root logger.
--- @param[optional] tag    - log tag, default is nil.
--- @param ...              - log message.
--- @returns - no return.
-function Log.e3(logger, tag, ...)
-    Log.output(3, logger, tag, ...)
-end
-
--- Log FATAL level message to root logger.
--- @param ... - log message.
--- @returns - no return.
-function Log.f(...)
-    Log.output(4, nil, nil, ...)
-end
-
--- Log FATAL level message to specific logger.
--- @param[optional] logger - logger name, default is root logger.
--- @param ...              - log message.
--- @returns - no return.
-function Log.f2(logger, ...)
-    Log.output(4, logger, nil, ...)
-end
-
--- Log FATAL level message to specific logger, and append tag.
--- @param[optional] logger - logger name, default is root logger.
--- @param[optional] tag    - log tag, default is nil.
--- @param ...              - log message.
--- @returns - no return.
-function Log.f3(logger, tag, ...)
-    Log.output(4, logger, tag, ...)
 end
 
 -- Output message to logger.
@@ -163,8 +50,11 @@ end
 function Log.output(level, logger, tag, ...)
     local file, line
     if Log.logFileInfo then
-        local di = debug.getinfo(3, 'Sl')
-        file, line = string.sub(di.source, 2), di.currentline
+        local di = debug_getinfo(3, 'Sl')
+        file, line = di.source, di.currentline
+        if Log.trimFilePrefix then
+            file = string_sub(file, 2)
+        end
     end
 
     if tag == nil then
@@ -179,12 +69,134 @@ function Log.output(level, logger, tag, ...)
     end
 
     if level >= 3 then -- For improve Log performance, explicit use Log Level value to perform if condition judge.
-        local logMsg = _llbc.Util_Table_Concat({...}, ' ')
-        local logMsgAppendedTb = debug.traceback(logMsg, 3)
-        _llbc.LogMsg(level, logger, tag, file, line, logMsgAppendedTb)
+        local logMsg = Util_Table_Concat({...}, ' ')
+        local logMsgAppendedTb = debug_traceback(logMsg, 3)
+        LogMsg(level, logger, tag, file, line, logMsgAppendedTb)
     else
-        _llbc.LogMsg(level, logger, tag, file, line, ...)
+        LogMsg(level, logger, tag, file, line, ...)
     end
+end
+
+local output = Log.output
+
+-- Log DEBUG level message to root logger.
+-- @param ... - log message.
+-- @returns - no return.
+function Log.d(...)
+    output(0, nil, nil, ...)
+end
+
+-- Log DEBUG level message to specific logger.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.d2(logger, ...)
+    output(0, logger, nil, ...)
+end
+
+-- Log DEBUG level message to specific logger, and append tag.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param[optional] tag    - log tag, default is nil.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.d3(logger, tag, ...)
+    output(0, logger, tag, ...)
+end
+
+-- Log INFO level message to root logger.
+-- @param ... - log message.
+-- @returns - no return.
+function Log.i(...)
+    output(1, nil, nil, ...)
+end
+
+-- Log INFO level message to specific logger.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.i2(logger, ...)
+    output(1, logger, nil, ...)
+end
+
+-- Log INFO level message to specific logger, and append tag.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param[optional] tag    - log tag, default is nil.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.i3(logger, tag, ...)
+    output(1, logger, tag, ...)
+end
+
+-- Log WARN level message to root logger.
+-- @param ... - log message.
+-- @returns - no return.
+function Log.w(...)
+    output(2, nil, nil, ...)
+end
+
+-- Log WARN level message to specific logger.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.w2(logger, ...)
+    output(2, logger, nil, ...)
+end
+
+-- Log WARN level message to specific logger.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param[optional] tag    - log tag, default is nil.
+-- @param ... -              log message.
+-- @returns - no return.
+function Log.w3(logger, tag, ...)
+    output(2, logger, tag, ...)
+end
+
+-- Log ERROR level message to root logger.
+-- @param ... - log message.
+-- @returns - no return.
+function Log.e(...)
+    output(3, nil, nil, ...)
+end
+
+-- Log ERROR level message to specific logger.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.e2(logger, ...)
+    output(3, logger, nil, ...)
+end
+
+-- Log ERROR level message to specific logger, and append tag.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param[optional] tag    - log tag, default is nil.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.e3(logger, tag, ...)
+    output(3, logger, tag, ...)
+end
+
+-- Log FATAL level message to root logger.
+-- @param ... - log message.
+-- @returns - no return.
+function Log.f(...)
+    output(4, nil, nil, ...)
+end
+
+-- Log FATAL level message to specific logger.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.f2(logger, ...)
+    output(4, logger, nil, ...)
+end
+
+-- Log FATAL level message to specific logger, and append tag.
+-- @param[optional] logger - logger name, default is root logger.
+-- @param[optional] tag    - log tag, default is nil.
+-- @param ...              - log message.
+-- @returns - no return.
+function Log.f3(logger, tag, ...)
+    output(4, logger, tag, ...)
 end
 
 -- Set Log table to llbc
