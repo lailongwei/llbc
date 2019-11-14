a69d0954e3ddd6dbf7f79f8c00c4fafe97f28896
lailongwei <lailongwei@126.com>
2019-11-14 09:34:07 +0800

    Fix epoll poller bug(on recv)

--- llbc/src/comm/EpollPoller.cpp
+++ llbc/src/comm/EpollPoller.cpp
@@ -237,7 +237,8 @@ void LLBC_EpollPoller::AddSession(LLBC_Session *session)
 
     LLBC_EpollCtl(_epoll, EPOLL_CTL_ADD, handle, &epev);
 
-    session->OnRecv();
+    if (sock->IsListen())
+        session->OnRecv();
 }
 
 void LLBC_EpollPoller::RemoveSession(LLBC_Session *session)
