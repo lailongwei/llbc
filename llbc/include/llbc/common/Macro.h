406a6e2caae2507c1df21199fd46855909aede49
lailongwei <lailongwei@126.com>
2021-12-13 10:28:00 +0800

    Fix windows编译错误

--- llbc/include/llbc/common/Macro.h
+++ llbc/include/llbc/common/Macro.h
@@ -329,7 +329,7 @@ private:                                            \
         }                                                           \
                                                                     \
         va_list ___llbc_macro_inl_argfmt_ap;                        \
-        va_start(___llbc_macro_inl_argfmt_ap, (fmt));               \
+        va_start(___llbc_macro_inl_argfmt_ap, fmt);                 \
         int ___llbc_macro_inl_argfmt_vsnp_len = ::vsnprintf(nullptr, 0, (fmt), ___llbc_macro_inl_argfmt_ap); \
         va_end(___llbc_macro_inl_argfmt_ap);                        \
         if (___llbc_macro_inl_argfmt_vsnp_len < 0) {                \
@@ -339,7 +339,7 @@ private:                                            \
         }                                                           \
                                                                     \
         buf = LLBC_Malloc(char, ___llbc_macro_inl_argfmt_vsnp_len + 1); \
-        va_start(___llbc_macro_inl_argfmt_ap, (fmt));               \
+        va_start(___llbc_macro_inl_argfmt_ap, fmt);                 \
         ___llbc_macro_inl_argfmt_vsnp_len = ::vsnprintf((buf), ___llbc_macro_inl_argfmt_vsnp_len + 1, (fmt), ___llbc_macro_inl_argfmt_ap); \
         va_end(___llbc_macro_inl_argfmt_ap);                        \
         if (___llbc_macro_inl_argfmt_vsnp_len < 0) {                \
