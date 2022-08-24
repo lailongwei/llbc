cf4a585d47cd18ec55fcea45241842210191d382
lailongwei <964855959@qq.com>
2022-08-24 12:59:49 +0800

    更新pylib_setting.lua

--- wrap/pyllbc/pylib_setting.lua
+++ wrap/pyllbc/pylib_setting.lua
@@ -1,6 +1,9 @@
 local pylib_setting = {
+    -- py_ver = 2, -- 2: python2x, 3: python3x, default is 2, Note: this setting option always used no matter 'use_setting' option.
     py_ver = 2, -- 2: python2x, 3: python3x, default is 2, Note: this setting option always used no matter 'use_setting' option.
-    py_path = {"", "", ""}, -- python path setting, 1: include path, 2: library path, 3: library name
+
+    -- py_path = {"", "", ""}, -- python path setting, 1: include path, 2: library path, 3: library name
+    py_path = {"", "", ""},
 }
 
 return pylib_setting
