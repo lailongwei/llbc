da941c3c9e53499ccb0f23d5c8f3543059ffc0a5
reckful <snakeflutes@gmail.com>
2024-03-07 11:58:00 +0800

    WIP: LLBC_Component方法收敛

--- wrap/pyllbc/src/comm/PyComponent.cpp
+++ wrap/pyllbc/src/comm/PyComponent.cpp
@@ -163,31 +163,31 @@ void pyllbc_Component::OnIdle(const LLBC_TimeSpan &idleTime)
     CallComponentMeth(_pyOnIdleMeth, _holdedOnIdleEv, false, false);
 }
 
-void pyllbc_Component::OnEvent(LLBC_ComponentEvents::ENUM event, const LLBC_Variant &evArgs)
+void pyllbc_Component::OnEvent(LLBC_ComponentEventType::ENUM event, const LLBC_Variant &evArgs)
 {
     switch(event)
     {
-        case LLBC_ComponentEvents::SessionCreate:
+        case LLBC_ComponentEventType::SessionCreate:
         {
             OnSessionCreate(*evArgs.AsPtr<LLBC_SessionInfo>());
             break;
         }
-        case LLBC_ComponentEvents::SessionDestroy:
+        case LLBC_ComponentEventType::SessionDestroy:
         {
             OnSessionDestroy(*evArgs.AsPtr<LLBC_SessionDestroyInfo>());
             break;
         }
-        case LLBC_ComponentEvents::AsyncConnResult:
+        case LLBC_ComponentEventType::AsyncConnResult:
         {
             OnAsyncConnResult(*evArgs.AsPtr<LLBC_AsyncConnResult>());
             break;
         }
-        case LLBC_ComponentEvents::ProtoReport:
+        case LLBC_ComponentEventType::ProtoReport:
         {
             OnProtoReport(*evArgs.AsPtr<LLBC_ProtoReport>());
             break;
         }
-        case LLBC_ComponentEvents::UnHandledPacket:
+        case LLBC_ComponentEventType::UnHandledPacket:
         {
             OnUnHandledPacket(*evArgs.AsPtr<LLBC_Packet>());
             break;
