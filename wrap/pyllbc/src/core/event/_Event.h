a30ffc4c1e0272e728de91c959682327e46603f2
reckful <snakeflutes@gmail.com>
2024-10-11 00:01:11 +0800

    feat: LLBC_Event key 更换为 LLBC_Variant 类型 & c++ 标准升级为 17

--- wrap/pyllbc/src/core/event/_Event.h
+++ wrap/pyllbc/src/core/event/_Event.h
@@ -53,7 +53,7 @@ LLBC_EXTERN_C PyObject *_pyllbc_Event_GetItem(PyObject *self, PyObject *args)
         return nullptr;
 
     // Try find event param from constant string_key params.
-    auto cit = ev->GetVariantKeyParams().find(LLBC_Variant[evParamKey]);
+    auto cit = ev->GetVariantKeyParams().find(LLBC_Variant(evParamKey));
     if (cit != ev->GetVariantKeyParams().end())
         return pyllbc_ObjUtil::Variant2Obj(cit->second);
 
