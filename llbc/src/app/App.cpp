75632cfc3b5fc4bbb8ff9d37167959334a2655c1
lailongwei <964855959@qq.com>
2024-11-22 19:43:33 +0800

    fix: 修正LLBC_App代码注释.

--- llbc/src/app/App.cpp
+++ llbc/src/app/App.cpp
@@ -220,7 +220,7 @@ int LLBC_App::Start(int argc, char *argv[], const LLBC_String &name)
             _cfgType = static_cast<LLBC_AppConfigType::ENUM>(cfgType);
     }
 
-    // Reload
+    // Reload.
     LLBC_ReturnIf(_cfgType != LLBC_AppConfigType::End && ReloadImpl(false, false) != LLBC_OK, LLBC_FAILED);
 
     // Handle progress crash.
@@ -513,7 +513,7 @@ LLBC_String LLBC_App::LocateConfigPath(const LLBC_String &appName, int &cfgType)
     if (execName.endswith("_d") || execName.endswith("_debug"))
         cfgFileNames.push_back(execName.substr(0, execName.rfind('_')));
 
-    // Execute config file locatee
+    // Execute config file locate.
     for (auto &cfgFileDir : cfgFileDirs)
     {
         for (auto &cfgFileName : cfgFileNames)
