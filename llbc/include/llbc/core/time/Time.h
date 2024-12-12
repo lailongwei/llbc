c07f8c406452db7ea93776581ecc3f88734d0095
lailongwei <964855959@qq.com>
2024-12-12 17:43:48 +0800

    fix: Fix LLBC_Time ostream operator<<重复定义问题

--- llbc/include/llbc/core/time/Time.h
+++ llbc/include/llbc/core/time/Time.h
@@ -29,11 +29,6 @@ class LLBC_Time;
 
 __LLBC_NS_END
 
-/**
- * Time class stream output operators previous declare.
- */
-LLBC_EXPORT std::ostream &operator<<(std::ostream &stream, const LLBC_NS LLBC_Time &t);
-
 __LLBC_NS_BEGIN
 
 /**
