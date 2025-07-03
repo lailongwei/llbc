50d03d09ca4467a860dbe96c08ac4575f87a2591
lailongwei <964855959@qq.com>
2025-07-03 14:53:33 +0800

    feat: 【llbc】LLBC_Directory::ModuleFilePath()能力扩展 #405

--- llbc/src/core/file/Directory_Darwin.mm
+++ llbc/src/core/file/Directory_Darwin.mm
@@ -11,11 +11,37 @@
 
  #include "llbc/core/file/Directory.h"
 
- #if LLBC_TARGET_PLATFORM_IPHONE
+// iPhone/Macosx implementation:
+#if LLBC_TARGET_PLATFORM_IPHONE || LLBC_TARGET_PLATFORM_MAC
 #import <Foundation/Foundation.h>
-
  __LLBC_NS_BEGIN
+LLBC_String LLBC_Directory::ModuleFilePath(bool readLink)
+{
+    if (readLink)
+    {
+        char buf[PATH_MAX + 1];
+        uint32 size = sizeof(buf);
+        if (_NSGetExecutablePath(buf, &size) != 0)
+        {
+            LLBC_SetLastError(LLBC_ERROR_CLIB);
+            return "";
+        }
 
+        return LLBC_String(buf, size);
+    }
+    else
+    {
+        NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+        NSString *executablePath = [[processInfo arguments] objectAtIndex:0];
+        return AbsPath([executablePath UTF8String]);
+    }
+}
+__LLBC_NS_END
+#endif // iPhone/Macosx
+
+// iPhone only implementation:
+ #if LLBC_TARGET_PLATFORM_IPHONE
+ __LLBC_NS_BEGIN
 LLBC_String LLBC_Directory::DocDir()
 {
     NSArray *docPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
@@ -41,7 +67,5 @@ LLBC_String LLBC_Directory::CacheDir()
     NSString *cachePath = [cachePaths objectAtIndex:0];
     return [cachePath UTF8String];
 }
-
  __LLBC_NS_END
-
  #endif // iPhone
