343eec51cb3c470a160f4c6742d8963909db1f41
corkiwang <corkiwang@tencent.com>
2025-08-28 11:58:31 +0800

    feat: crash handle支持数组 #426

--- testsuite/TestSuite.cpp
+++ testsuite/TestSuite.cpp
@@ -49,7 +49,9 @@
 int TestSuite_Main(int argc, char* argv[])
 {
     LLBC_Startup();
-    LLBC_HandleCrash();
+
+    LLBC_ReturnIf(LLBC_EnableCrashHandle() != LLBC_OK, LLBC_FAILED);
+
     __TraitsLoop<__TEST_CASE_COUNT>::Generate();
 
     while (true)
